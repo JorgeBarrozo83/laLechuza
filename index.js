7c764007bb0f73c219658470c30e9f5e3245bdcc
Jorge Barrozo <jorgeluisbarrozo@hotmail.com.ar>
2022-11-05 17:41:51 -0300

    array y objetos

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 alert("¡Bienvenido a Cabañas La Lechuza. Bienvenido a Potrerillos!");
+const cabanasDisponibles=[]
 class Cabanas {
     constructor(nombre, id, precio) {
         this.nombre = nombre;
@@ -7,8 +8,11 @@ class Cabanas {
     }
 }
 const retama = new Cabanas("retama", 1, 8000);
+cabanasDisponibles.push(retama);
 const amapola = new Cabanas("amapola", 2, 10000);
+cabanasDisponibles.push(amapola);
 const lechuza = new Cabanas("lechuza", 3, 14000);
+cabanasDisponibles.push(lechuza);
 
 let cabanaElegida = parseInt(prompt("Elije la cabaña por la cantidad de personas 1.Retama(2 pers.) - 2.Amapola (3 o 4 pers.) - 3.Lechuza (5 o 6 pers.) "))
 
@@ -33,17 +37,17 @@ while (elijeCabana === false) {
 }
 
 console.log("cabaña elejida", cabanaQueToca);
-let presupuesto
 
+let presupuesto=0
 let diasVacaciones = parseInt(prompt("¿Cuantos dias quieres hospedarte?"));
-
+console.log(diasVacaciones)
 costoEstadia(diasVacaciones, cabanaQueToca)
 
 function costoEstadia(dias, cabana) {
-    let presupuesto = dias * cabana.precio
+     presupuesto = dias * cabana.precio
 
     return presupuesto
     
 }
 let presupuestoFinal= costoEstadia(diasVacaciones, cabanaQueToca);
-alert (`Tu presupuesto de la cabaña ${cabanaQueToca} por ${diasVacaciones} es de $ ${presupuesto}`);
+alert (`Tu presupuesto de la cabaña ${cabanaQueToca.nombre} por ${diasVacaciones} dias es de $ ${presupuesto}`);
