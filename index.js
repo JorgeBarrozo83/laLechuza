5b5ba57ba29d570da344cebe2b792b9d7b6f0703
Jorge Barrozo <jorgeluisbarrozo@hotmail.com.ar>
2022-11-21 18:45:03 -0300

    h3 no funciona

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-//alert("¡Bienvenido a Cabañas La Lechuza. Bienvenido a Potrerillos!");
-const inputNombre= document.getElementById("nombre")
-const inputApellido= document.getElementById("apellido")
-const inputPersonas= document.getElementById("personas")
-const inputDias= document.getElementById("dias")
-const botonConfirmar= document.getElementById("confirmar")
-const divSeleccion= document.getElementById("seleccion")
-const divAsignacion= document.getElementById("asignacion")
+const inputNombre = document.getElementById("nombre");
+const inputApellido = document.getElementById("apellido");
+const inputPersonas = document.getElementById("personas");
+const inputDias = document.getElementById("dias");
+const botonConfirmar = document.getElementById("confirmar");
+const divSeleccion = document.getElementById("seleccion");
+const divAsignacion = document.getElementById("asignacion");
 
-botonConfirmar.onclick =() => {
-    if(inputNombre.value && inputApellido.value && inputPersonas.value && inputDias){
-    const huesped={
-        nombre: inputNombre.value ,
-        apellido: inputApellido.value ,
-        personas: inputPersonas.value ,
-        dias: inputDias.value ,
-    }
-    localStorage.setItem("infoHuesped" , JSON.stringify (huesped))
-}
-divSeleccion.remove()
-const cabanaAsignada= document.createElement("h3")
-cabanaAsignada.innerText= `${usario.nombre} ${usuario.apellido} segun tu seleccion, tu presupuesto por ${usuario.personas} personas y ${usuario.dias} es de ${presupuesto}`
-divAsignacion.append(cabanaAsignada)
+botonConfirmar.onclick = () => {
+  if (
+    inputNombre.value &&
+    inputApellido.value &&
+    inputPersonas.value &&
+    inputDias
+  ) {
+    const huesped = {
+      nombre: inputNombre.value,
+      apellido: inputApellido.value,
+      personas: inputPersonas.value,
+      dias: inputDias.value,
+    };
+    localStorage.setItem("infoHuesped", JSON.stringify(huesped));
+  }
+  divSeleccion.remove();
+  const cabanaAsignada = document.createElement("h3");
+  cabanaAsignada.innerText = `${huesped.nombre} ${huesped.apellido} según tu seleccion, tu presupuesto por ${huesped.personas} personas y ${huesped.dias} es de ${presupuesto}`;
+  divAsignacion.append(cabanaAsignada);
+  const presupuesto = 0;
+};
+inputPersonas;
 
-let presupuesto=0
-}
 
-let presupuesto=0
-
-
-const cabanasDisponibles=[]
+const cabanasDisponibles = [];
 class Cabanas {
-    constructor(nombre, id, precio) {
-        this.nombre = nombre;
-        this.id = id;
-        this.precio = precio;
-    }
+  constructor(nombre, id, precio) {
+    this.nombre = nombre;
+    this.id = id;
+    this.precio = precio;
+  }
 }
 const retama = new Cabanas("Retama", 1, 8000);
 cabanasDisponibles.push(retama);
@@ -43,29 +45,7 @@ cabanasDisponibles.push(amapola);
 const lechuza = new Cabanas("Lechuza", 3, 14000);
 cabanasDisponibles.push(lechuza);
 
-//let cabanaElegida = parseInt(prompt("Elije la cabaña por la cantidad de personas 1.Retama(2 pers.) - 2.Amapola (3 o 4 pers.) - 3.Lechuza (5 o 6 pers.) "))
 
-//let cabanaQueToca
-
-
-//let elijeCabana = false;
-
-//while (elijeCabana === false) {
-   // if (cabanaElegida === retama.id) {
-       // elijeCabana = true
-       // cabanaQueToca = retama
-   // } else if (cabanaElegida === amapola.id) {
-        //elijeCabana = true
-        //cabanaQueToca = amapola
-    //} else if (cabanaElegida === lechuza.id) {
-        //elijeCabana = true
-        //cabanaQueToca = lechuza
-   // } else {
-       // cabanaElegida = parseInt(prompt("Elije la cabaña por la cantidad de personas 1.Retama(2 pers.) - 2.Amapola (3 o 4 pers.) - 3.Lechuza (5 o 6 pers.) "))
-   // }
-//}
-
-//console.log("cabaña elejida", cabanaQueToca);
 
 //let presupuesto=0
 //let diasVacaciones = parseInt(prompt("¿Cuantos dias quieres hospedarte?"));
@@ -73,10 +53,10 @@ cabanasDisponibles.push(lechuza);
 //costoEstadia(diasVacaciones, cabanaQueToca)
 
 //function costoEstadia(dias, cabana) {
-     //presupuesto = dias * cabana.precio
+//presupuesto = dias * cabana.precio
 
-    //return presupuesto
-    
+//return presupuesto
+
 //}
 //let presupuestoFinal= costoEstadia(diasVacaciones, cabanaQueToca);
 //alert (`Tu presupuesto de la cabaña ${cabanaQueToca.nombre} por ${diasVacaciones} dias es de $ ${presupuesto}`);
