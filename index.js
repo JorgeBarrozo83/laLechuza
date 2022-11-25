10030483c1bb39a83707135ad48dc56ae64108d4
Jorge Barrozo <jorgeluisbarrozo@hotmail.com.ar>
2022-11-25 18:42:01 -0300

    pre

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
 const botonConfirmar = document.getElementById("confirmar");
 
-botonConfirmar.addEventListener('click', () => {
-
-
+botonConfirmar.addEventListener("click", () => {
   const divSeleccion = document.getElementById("seleccion");
   const divAsignacion = document.getElementById("Asignacion");
   const inputNombre = document.getElementById("nombre").value;
   const inputApellido = document.getElementById("apellido").value;
   const inputPersonas = document.getElementById("personas").value;
   const inputDias = document.getElementById("dias").value;
- 
-    const huesped = {
-      nombre: inputNombre,
-      apellido: inputApellido,
-      personas: inputPersonas,
-      dias: inputDias,
-    };
-    console.log(huesped)
-    localStorage.setItem("infoHuesped", JSON.stringify(huesped));
+
+  const huesped = {
+    nombre: inputNombre,
+    apellido: inputApellido,
+    personas: inputPersonas,
+    dias: inputDias,
+  };
+  console.log(huesped);
+  localStorage.setItem("infoHuesped", JSON.stringify(huesped));
+
+  divSeleccion.remove();
+
   
-
-  divSeleccion.remove()
-
- const presupuesto = 0;
- 
+  
+  function costoEstadia(dia, cabana){
+    presupuesto= cabanasDisponibles.precio*huesped.dias
+    return presupuesto
+  }
+  const presupuesto= presupuesto+costoEstadia
+  
+ console.log(presupuesto)
   const cabanaAsignada = document.createElement("h3");
   cabanaAsignada.innerText = `${huesped.nombre} ${huesped.apellido} según tu seleccion, tu presupuesto por ${huesped.personas} por ${huesped.dias} dias es de ${presupuesto}`;
   divAsignacion.append(cabanaAsignada);
- 
+
+  Swal.fire({
+    title: ` ${huesped.personas}`,
+    text: `${huesped.nombre} ${huesped.apellido} según tu seleccion, tu presupuesto por ${huesped.personas} por ${huesped.dias} dias es de ${presupuesto}`,
+    imageUrl: (src = "./img/cabana-2dorm-3.jpg"),
+    imageWidth: 500,
+    imageHeight: 300,
+    imageAlt: "Cabaña Asignada",
+  });
+});
 
 
-
-})
 
 const cabanasDisponibles = [];
 class Cabanas {
@@ -47,8 +58,6 @@ const amapola = new Cabanas("Amapola", 2, 10000);
 cabanasDisponibles.push(amapola);
 const lechuza = new Cabanas("Lechuza", 3, 14000);
 cabanasDisponibles.push(lechuza);
-
-
 
 //let presupuesto=0
 //let diasVacaciones = parseInt(prompt("¿Cuantos dias quieres hospedarte?"));
