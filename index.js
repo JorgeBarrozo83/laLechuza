792f4343e557656db7888a9b56b2a0b6cb439536
Jorge Barrozo <jorgeluisbarrozo@hotmail.com.ar>
2022-12-08 10:05:46 -0300

    presupuesto

--- index.js
+++ index.js
@@ -1,82 +1,121 @@
+Swal.fire({
+  title: "Bienvenido",
+  text: "Al darler OK veras una vista previa de nuestras cabañas y su capacidad. Pulsa confirmar para tener mas info",
+  imageUrl: (src = "./img/Potrerillos (1).jpg"),
+  imageWidth: 500,
+  imageHeight: 300,
+  imageAlt: "Presentación",
+});
+
+const divCards = document.querySelector(".cards");
+
+const mostrarCabanas = async () => {
+  const cabanasFectch = await fetch("./cabanas.json");
+  const cabanasJson = await cabanasFectch.json();
+  console.log(cabanasJson);
+  cabanasJson.forEach((cab) => {
+    const { nombre, id, precio, prebajo, img, descripcion } = cab;
+    divCards.innerHTML += `
+     <div class="card" style="width: 4rem;">
+   <img src="${img}" >
+   <div class="card-body">
+     <h4 class="card-title">${nombre}</h4>
+     <p>${descripcion}</p>
+     <button id="seleccionar-${id}">SELECCIONAR</button>
+   </div>
+ </div>
+ `;
+
+ 
+  });
+
+  cabanasJson.forEach((cab) => {
+    const botonSeleccionar = document.getElementById(`seleccionar-${cab.id}`);
+    botonSeleccionar.addEventListener("click", () => {
+      localStorage.setItem("cabElejida",JSON.stringify(cab))
+      
+      console.log(cab)
+      divCards.remove();
+    });
+  });
+};
+mostrarCabanas();
+
 const botonConfirmar = document.getElementById("confirmar");
- const checkIn= document.getElementById("checkIn");
- const checkOut= document.getElementById("checkOut");
 
- botonConfirmar.addEventListener("click", () => {
-   const divSeleccion = document.getElementById("seleccion");
-   const divAsignacion = document.getElementById("Asignacion");
-   const inputNombre = document.getElementById("nombre").value;
-const inputApellido = document.getElementById("apellido").value;
-   const inputPersonas = document.getElementById("personas").value;
-   const inputDias = document.getElementById("dias").value;
+botonConfirmar.addEventListener("click", () => {
+  const divSeleccion = document.getElementById("seleccion");
+  const inputNombre = document.getElementById("nombre");
+  const inputApellido = document.getElementById("apellido");
+  const inputMes = document.getElementById("mes");
+  const inputDias = document.getElementById("dias");
 
-   const huesped = {
-     nombre: inputNombre,
-     apellido: inputApellido,
-     personas: inputPersonas,
-     dias: inputDias,
-   };
-   console.log(huesped);
-   localStorage.setItem("infoHuesped", JSON.stringify(huesped));
-
-   divSeleccion.remove();
+  const huesped = {
+    nombre: inputNombre.value,
+    apellido: inputApellido.value,
+    mes: inputMes.value,
+    dias: inputDias.value,
+  };
+  localStorage.setItem("infoReserva",JSON.stringify (huesped))
+  console.log(huesped)
   
- 
- 
-  const presupuesto=0
- 
-   const cabanaAsignada = document.createElement("h3");
-   cabanaAsignada.innerText = `${huesped.nombre} ${huesped.apellido} según tu seleccion, tu presupuesto por ${huesped.personas} por ${huesped.dias} dias es de ${presupuesto}`;
-   divAsignacion.append(cabanaAsignada);
 
- 
-
-   Swal.fire({
-     title: ` ${huesped.personas}`,
-     text: `${huesped.nombre} ${huesped.apellido} según tu seleccion, tu presupuesto por ${huesped.personas} por ${huesped.dias} dias es de ${presupuesto}`,
-     imageUrl: (src = "./img/cabana-2dorm-3.jpg"),
-     imageWidth: 500,
-     imageHeight: 300,
-     imageAlt: "Cabaña Asignada",
-   });
- });
- let fecha1= new Date(checkIn)
- let fecha2= new Date(checkOut)
- let milisegundosDia= 24 * 60 *60 *1000;
- let milisegundosTrancurridos= Math.abs(fecha1.getTime()- fecha2.getTime());
- let diasHospedaje= Math.round(milisegundosTrancurridos/milisegundosDia);
- console.log(diasHospedaje)
-
- function tarifa() {
-   if(diasHospedaje>=7){
-     return cabanasDisponibles.prebajo
-   } else{
-      cabanasDisponibles.precio
-   }
- }
+  
+});
+const objHuesped = JSON.parse(localStorage.getItem("infoReserva"));
+const objCabana= JSON.parse(localStorage.getItem("cabElejida"));
+  
+const presupuesto = 0;
+function tarifa(objHuesped, objCabana) {
+  objHuesped.dias >= 6 && objHuesped.mes < 4
+    ? objCabana.precio * objHuesped.dias
+    : objCabana.prebajo * objHuesped.dias;
+}
+const presupuestoFinal = presupuesto + tarifa;
 
 
+console.log(presupuestoFinal)
+// //     console.log(huesped);
+// //     localStorage.setItem("infoHuesped", JSON.stringify(huesped));
+//    divSeleccion.remove();
 
 const cabanasDisponibles = [];
 class Cabanas {
-  constructor(nombre, id, precio, prebajo, img) {
+  constructor(nombre, id, precio, prebajo, img, descripcion) {
     this.nombre = nombre;
     this.id = id;
     this.precio = precio;
-    this.prebajo= prebajo;
-    this.img=img;
+    this.prebajo = prebajo;
+    this.img = img;
+    this.descripcion = descripcion;
   }
 }
-const retama = new Cabanas("Retama", 1, 8000, 6000, "./img/cabana-2dorm-4-1.jpg");
+const retama = new Cabanas(
+  "Retama",
+  1,
+  8000,
+  6000,
+  (src = "./img/cabana-2dorm-4-1.jpg"),
+  "Cabaña para dos personas"
+);
 cabanasDisponibles.push(retama);
-const amapola = new Cabanas("Amapola", 2, 10000, 8000, "./img/cabana-1dorm-6-1.jpg");
+const amapola = new Cabanas(
+  "Amapola",
+  2,
+  10000,
+  8000,
+  (src = "./img/cabana-1dorm-6-1.jpg"),
+  "Cabaña para cuatro personas"
+);
 cabanasDisponibles.push(amapola);
-const lechuza = new Cabanas("Lechuza", 3, 14000, 12000, "./img/cabana-2dorm-3.jpg");
+const lechuza = new Cabanas(
+  "Lechuza",
+  3,
+  14000,
+  12000,
+  (src = "./img/cabana-2dorm-3.jpg"),
+  "Cabaña para seis personas"
+);
 cabanasDisponibles.push(lechuza);
 
-console.log(JSON.stringify(cabanasDisponibles))
-
-
-//const productosCabanas= await fetch(
- // "cabanas.json"
- // )
+//console.log(JSON.stringify(cabanasDisponibles))
